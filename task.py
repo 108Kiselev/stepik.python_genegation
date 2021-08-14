843d5119ecdd8a5600f89d0a5f0aa2412c094aef
108Kiselev <kiselev290@gmail.com>
2021-08-14 15:56:50 +0300

    2.2.1 определение четверти по 2 числам (x, y)

--- task.py
+++ task.py
@@ -1,10 +1,34 @@
 n = int(input())
-k = int(input())
-arr = []
-for i in range(1, n+1):
-    arr.append(i)
-i = 0
-while len(arr) > 1:
-     i = (i+(k)-1)%(len(arr))
-     arr.remove(arr[i])
-print(arr[0])
+
+res = [0, 0, 0, 0, 0]
+dot = []
+
+def define(x, y):
+    if x > 0:
+        if y > 0:
+            return 1
+        elif y < 0:
+            return 4
+        else:
+            return 0
+    elif x < 0:
+        if y > 0:
+            return 2
+        elif y < 0:
+            return 3
+        else: 
+            return 0
+    else:
+        return 0
+
+for i in range(n):
+    dot = input().split(' ')
+    for i in range(len(dot)):
+        dot[i] = int(dot[i])
+    out = define(dot[0], dot[1])
+    res[out] += 1
+
+print('Первая четверть:', res[1])
+print('Вторая четверть:', res[2])
+print('Третья четверть:', res[3])
+print('Четвертая четверть:', res[4])
