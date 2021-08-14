691bbfda115d8faa423a44d5cbd219f41895db41
108Kiselev <kiselev290@gmail.com>
2021-08-14 11:31:47 +0300

    2.1.3 цена строки

--- task.py
+++ task.py
@@ -1,4 +1,7 @@
-a = int(input())
-b = int(input())
-for i in range(b):
-    print(a + i)
+st = input()
+count = 0
+for i in range(len(st)):
+    count += 0.6
+    count = round(count, 2)
+print(int(count), "р. ", end='')
+print(int(count * 100 % 100), "коп.")
