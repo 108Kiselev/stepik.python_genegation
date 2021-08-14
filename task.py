bb6abef834bff6ef7f821268e67cfee5b518dfa2
108Kiselev <kiselev290@gmail.com>
2021-08-14 16:39:04 +0300

    2.2.4 циклический здвиг в списке

--- task.py
+++ task.py
@@ -1,11 +1,5 @@
 arr = list(input().split(' '))
-end = ''
-if len(arr) % 2 != 0:
-    end = arr.pop()
-for i in range(0, len(arr)-1, 2):
-    #print(arr[i], arr[i+1])
-    arr[i], arr[i+1] = arr[i+1], arr[i]
-for a in arr:
-    print(a, end = ' ')
-if end.isdigit():
-    print(end)
+a = arr.pop(-1)
+arr.insert(0, a)
+for i in arr:
+    print(i, end=' ')
