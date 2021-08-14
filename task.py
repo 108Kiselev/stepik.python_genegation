9e3909f215083782968e813802e5f205feda32db
108Kiselev <kiselev290@gmail.com>
2021-08-14 16:28:56 +0300

    2.2.3 замена следующего и текущего элем списка

--- task.py
+++ task.py
@@ -1,34 +1,11 @@
-n = int(input())
-
-res = [0, 0, 0, 0, 0]
-dot = []
-
-def define(x, y):
-    if x > 0:
-        if y > 0:
-            return 1
-        elif y < 0:
-            return 4
-        else:
-            return 0
-    elif x < 0:
-        if y > 0:
-            return 2
-        elif y < 0:
-            return 3
-        else: 
-            return 0
-    else:
-        return 0
-
-for i in range(n):
-    dot = input().split(' ')
-    for i in range(len(dot)):
-        dot[i] = int(dot[i])
-    out = define(dot[0], dot[1])
-    res[out] += 1
-
-print('Первая четверть:', res[1])
-print('Вторая четверть:', res[2])
-print('Третья четверть:', res[3])
-print('Четвертая четверть:', res[4])
+arr = list(input().split(' '))
+end = ''
+if len(arr) % 2 != 0:
+    end = arr.pop()
+for i in range(0, len(arr)-1, 2):
+    #print(arr[i], arr[i+1])
+    arr[i], arr[i+1] = arr[i+1], arr[i]
+for a in arr:
+    print(a, end = ' ')
+if end.isdigit():
+    print(end)
