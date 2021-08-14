2653af8736dff2ba63aac490ed162e832be4e1cd
108Kiselev <kiselev290@gmail.com>
2021-08-14 13:05:13 +0300

    2.1.6 реверс строки с числами

--- task.py
+++ task.py
@@ -1,6 +1,13 @@
-anim = ['Обезьяна', 'Петух', 'Собака', 'Свинья', 'Крыса', 'Бык', 'Тигр', 'Заяц', 'Дракон', 'Змея', 'Лошадь', 'Овца']
-year = int(input())
-i = year % 12
-print(anim[i])
-#0 year - monkey
-#1 year - cockerel
+num = input()
+st = ''
+if len(num) == 5:
+    for i in range(len(num)-1, -1, -1):
+        st += num[i]
+    print(int(st))
+elif len(num) == 6:
+    print(num[0], end='')
+    for i in range(len(num)-1, 0, -1):
+        st += num[i]
+    print(st)
+else:
+    print('Длина числа не соответствует условию!')
