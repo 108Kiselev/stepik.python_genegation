7b943e514f6a9671804d8b1c78fd638e2961781d
108Kiselev <kiselev290@gmail.com>
2021-08-15 19:15:28 +0300

    4.3.2 список по образцу

--- task.py
+++ task.py
@@ -1,18 +1,9 @@
-st = input()
-st += ' запретил букву'
+n = int(input())
 
-letters = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з',
-         'и', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 
-         'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 
-         'ъ', 'ы', 'ь', 
-         'э', 'ю', 'я']
+origArr = []
+inArr = []
 
-for i in range(len(letters)):
-    if letters[i] not in st:
-        continue
-    else:
-        if st[0] == ' ':
-            st = st.replace(' ', '', 1)
-        st = st.replace('  ', ' ', 3).strip()
-        print(st, letters[i])
-        st = st.replace(letters[i], '')
+for i in range(1, n+1):
+    inArr.append(i)
+    origArr.append(inArr)
+print(*origArr, sep='\n')
