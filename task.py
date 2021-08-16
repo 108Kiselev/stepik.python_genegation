eedcb08da4ff237e100804ff4a0e355adfdb4c8f
108Kiselev <kiselev290@gmail.com>
2021-08-16 17:11:42 +0300

    4.4.2 вывести матрицу 2

--- task.py
+++ task.py
@@ -3,3 +3,10 @@ mat = [[input() for i in range(m)] for j in range(n)]
 
 for i in mat:
     print(*i)
+
+print()
+
+for i in range(m):
+    for j in range(n):
+        print(mat[j][i], end = ' ')
+    print()
