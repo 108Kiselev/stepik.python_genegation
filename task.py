160a18fbbbb5ff2e1a5c0648ddf27e3d311cfaa8
108Kiselev <kiselev290@gmail.com>
2021-08-16 10:58:04 +0300

    4.3.5 упаковка дубликатов

--- task.py
+++ task.py
@@ -1,12 +1,16 @@
-n = int(input())
+st = input()
+st = st.replace(' ', '', 500) + '0'
 
-pas = [[1], [1, 1], [1, 2, 1]]
+sli = []
+res = []
 
-for i in range(3, n+1):
-    row = []
-    row.append(1)
-    for j in range(1, len(pas[i-1])):
-        row.append(pas[-1][j-1] + pas[-1][j])
-    row.append(1)
-    pas.append(row)
-print(pas[n])
+sli.append(st[0])
+
+for i in range(len(st)-1):
+    if st[i] == st[i+1]:
+        sli.append(st[i+1])
+    else:
+        res.append(sli)
+        sli = []
+        sli.append(st[i+1])
+print(res)
