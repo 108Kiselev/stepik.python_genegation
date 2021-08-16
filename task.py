fb55ec3a8b5ae4b985ea572239766095fc597a1a
108Kiselev <kiselev290@gmail.com>
2021-08-16 16:28:03 +0300

    4.4.1 вывести матрицу 1

--- task.py
+++ task.py
@@ -1,20 +1,5 @@
-lis = input().split()
+n, m = int(input()), int(input())
+mat = [[input() for i in range(m)] for j in range(n)]
 
-res = [[]]
-
-for i in lis:
-    res.append(list(i))
-
-i = 0
-j = 2
-count = 1
-
-while j <= len(lis):
-    while j <= len(lis):
-        res.append(lis[i:j])
-        i += 1
-        j += 1
-    i = 0
-    j = 2 + count
-    count += 1
-print(res)
+for i in mat:
+    print(*i)
