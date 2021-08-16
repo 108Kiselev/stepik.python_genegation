39e8897efc73e9dd81455cf9863d7427cccc272c
108Kiselev <kiselev290@gmail.com>
2021-08-16 15:25:22 +0300

    4.3.7 подсписки списка

--- task.py
+++ task.py
@@ -1,17 +1,20 @@
-def chunked(lis , n):
-    res = []
-    i = 0
-    m = n
-
-    while True:
-        res.append(lis[i:m])
-        i += n
-        m += n
-        if i >= len(lis):
-            break
-    return res
-
 lis = input().split()
-n = int(input())
 
-print(chunked(lis , n))
+res = [[]]
+
+for i in lis:
+    res.append(list(i))
+
+i = 0
+j = 2
+count = 1
+
+while j <= len(lis):
+    while j <= len(lis):
+        res.append(lis[i:j])
+        i += 1
+        j += 1
+    i = 0
+    j = 2 + count
+    count += 1
+print(res)
