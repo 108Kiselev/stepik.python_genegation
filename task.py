dfa4cf217cf3e00ccfa85be3f1c4929becfb4882
108Kiselev <kiselev290@gmail.com>
2021-08-16 20:28:00 +0300

    4.4.7 суммы четвертей

--- task.py
+++ task.py
@@ -1,13 +1,22 @@
 n = int(input())
 mat = list(list(map(int, input().split())) for i in range(n))
 
-left = []
-right = []
+left = 0
+right = 0
+up = 0
+down = 0
 
 for i in range(n):
     for j in range(n):
-        if i >= j and i <= n - 1 - j:
-            left.append(mat[i][j])
-        elif i <= j and i >= n - 1 - j:
-            right.append(mat[i][j])
-print(max(max(left), max(right)))
+        if i > j and i < n - 1 - j:
+            left += mat[i][j]
+        elif i < j and i > n - 1 - j:
+            right += mat[i][j]
+        elif i < j and i < n - 1 - j:
+            up += mat[i][j]
+        elif i > j and i > n - 1 - j:
+            down += mat[i][j]
+print('Верхняя четверть:', up)
+print('Правая четверть:', right)
+print('Нижняя четверть:', down)
+print('Левая четверть:', left)
