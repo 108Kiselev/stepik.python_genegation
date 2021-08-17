3a0634484bfb59388e051d0002a5d12323b97bc4
108Kiselev <kiselev290@gmail.com>
2021-08-17 11:56:21 +0300

    4.5.1 таблица умножения

--- task.py
+++ task.py
@@ -1,22 +1,9 @@
-n = int(input())
-mat = list(list(map(int, input().split())) for i in range(n))
+n, m = int(input()), int(input())
+mult = [[0 for i in range(m)] for j in range(n)]
 
-left = 0
-right = 0
-up = 0
-down = 0
+for i in range(len(mult)):
+    for j in range(len(mult[i])):
+        mult[i][j] = i * j
 
-for i in range(n):
-    for j in range(n):
-        if i > j and i < n - 1 - j:
-            left += mat[i][j]
-        elif i < j and i > n - 1 - j:
-            right += mat[i][j]
-        elif i < j and i < n - 1 - j:
-            up += mat[i][j]
-        elif i > j and i > n - 1 - j:
-            down += mat[i][j]
-print('Верхняя четверть:', up)
-print('Правая четверть:', right)
-print('Нижняя четверть:', down)
-print('Левая четверть:', left)
+for m in mult:
+    print(*m)
