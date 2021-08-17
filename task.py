48cb6486a5eb2236308299ca4dc2d14d286c3d10
108Kiselev <kiselev290@gmail.com>
2021-08-17 16:35:50 +0300

    4.5.6 горизонтальное отражение матрицы отн оси

--- task.py
+++ task.py
@@ -1,10 +1,20 @@
-n = int(input())
-mat = list(list(map(int, input().split())) for i in range(n))
-j = -1
+'''n = int(input())
+mat = list(list(map(int, input().split())) for i in range(n))'''
+def fun(n, mat):
+    j = -1
 
-for i in range(n):
-    mat[i][i], mat[j][i] = mat[j][i], mat[i][i]
-    j -= 1
+    for i in range(n//2):
+        mat[i], mat[j] = mat[j], mat[i]
+        j -= 1
 
-for i in mat:
-    print(*i)
+    for i in mat:
+        print(*i)
+    
+fun(4, [[1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [8, 6, 4, 2],
+        [3, 4, 5, 6]])
+print()
+fun(3, [[1, 2, 3],
+        [5, 6, 7],
+        [8, 6, 4]])
