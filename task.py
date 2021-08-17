b9c39b7107609a3257034257288ae304f2dd20b1
108Kiselev <kiselev290@gmail.com>
2021-08-17 16:48:05 +0300

    4.5.7 поворот матрицы на 90 град

--- task.py
+++ task.py
@@ -1,14 +1,16 @@
 '''n = int(input())
 mat = list(list(map(int, input().split())) for i in range(n))'''
+
 def fun(n, mat):
-    j = -1
+    submat = [['0'] * n for i in range(n)]
+    for i in range(n):
+        for j in range(len(mat)):
+            submat[i][j] = mat[j][i]
 
-    for i in range(n//2):
-        mat[i], mat[j] = mat[j], mat[i]
-        j -= 1
-
-    for i in mat:
-        print(*i)
+    for i in submat:
+        for j in range(-1, -n-1, -1):
+            print(i[j], end = ' ')
+        print()
     
 fun(4, [[1, 2, 3, 4],
         [5, 6, 7, 8],
