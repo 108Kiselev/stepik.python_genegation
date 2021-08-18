11b6ad2973e638f6e285acb3b9120867b1371099
108Kiselev <kiselev290@gmail.com>
2021-08-18 10:36:49 +0300

    4.5.8 определение ходов коня в шахматах

--- task.py
+++ task.py
@@ -1,22 +1,31 @@
 '''n = int(input())
 mat = list(list(map(int, input().split())) for i in range(n))'''
 
-def fun(n, mat):
-    submat = [['0'] * n for i in range(n)]
-    for i in range(n):
-        for j in range(len(mat)):
-            submat[i][j] = mat[j][i]
+def fun(n):
+    keys = {'a': 0,
+            'b': 1,
+            'c': 2,
+            'd': 3,
+            'e': 4,
+            'f': 5,
+            'g': 6,
+            'h': 7}
+    mat = [['.'] * 8 for i in range(9)]
+    y = int(n[1])
+    x = int(keys.get(n[0]))
+    mat[y][x] = 'N'
+    '''
+    y from 1, j
+    x from 0 i
+    '''
+    for i in range(8):
+        for j in range(1, 9):
+            if j == y + 2 and i == x - 1 or j == y + 2 and i == x + 1 or j == y + 1 and i == x + 2 or j == y - 1 and i == x + 2 or j == y - 2 and i == x + 1 or j == y - 2 and i == x - 1 or j == y - 1 and i == x - 2 or j == y + 1 and i == x - 2:
+                mat[j][i] = '*'
 
-    for i in submat:
-        for j in range(-1, -n-1, -1):
-            print(i[j], end = ' ')
-        print()
+    for i in range(-1, -len(mat), -1):
+        print(*mat[i])
     
-fun(4, [[1, 2, 3, 4],
-        [5, 6, 7, 8],
-        [8, 6, 4, 2],
-        [3, 4, 5, 6]])
+fun('b6')
 print()
-fun(3, [[1, 2, 3],
-        [5, 6, 7],
-        [8, 6, 4]])
+fun('f3')
