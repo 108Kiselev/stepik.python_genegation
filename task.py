6760bd56e319241b6a18db4d78cd3f00082c050f
108Kiselev <kiselev290@gmail.com>
2021-08-18 12:02:01 +0300

    4.6.1 звездочки в шахматном порядке

--- task.py
+++ task.py
@@ -1,51 +1,24 @@
-'''n = int(input())
-mat = list(list(map(int, input().split())) for i in range(n))'''
+'''inp = input().split()
+n = int(inp[0])
+m = int(inp[1])'''
 
-def fun(n, mat):
-    flag = 0
-    for i in range(1, n**2 + 1):
-        for j in mat:
-            if i in j:
-                flag = 1
-        if not flag: 
-            print('NO')
-            return
-        flag = 0
 
-    summ = sum(mat[0])
+def fun(n, m):
+    mat = [['.'] * m for i in range(n)]
 
+    for i in range(n):
+        for j in range(m):
+            if i % 2 == 1:
+                if j % 2 != 1:
+                    mat[i][j] = '*'
+            else:
+                if j % 2 == 1:
+                    mat[i][j] = '*'
     for i in mat:
-        if summ != sum(i):
-            print('NO')
-            return
-
-    for i in range(n):
-        subsumm = 0
-        for j in range(n):
-            subsumm += mat[j][i]
-        if subsumm != summ:
-            print('NO')
-            return
-
-    summd = 0
-    for i in range(n):
-        summd += mat[i][i]
-    if summd != summ:
-        print('NO')
-        return
+        print(*i)
     
-    summd = 0
-    for i in range(n):
-        summd += mat[i-n][i]
-    if summd != summ:
-        print('NO')
-        return
-    print('YES')
-    
-fun(3, [[8, 2, 6,],
-        [3, 5, 7,],
-        [4, 9, 1]])
+fun(3, 4)
 print()
-fun(3, [[0, 1, 6,],
-        [3, 5, 7,],
-        [4, 9, 2]])
+fun(2, 2)
+print()
+fun(1, 8)
