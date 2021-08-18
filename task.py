5a5d03e9d20264b4e196dbfbd3af5e5c530449b0
108Kiselev <kiselev290@gmail.com>
2021-08-18 11:09:22 +0300

    4.5.9 проверка суммы элементов стр, столб, диаг

--- task.py
+++ task.py
@@ -1,31 +1,51 @@
 '''n = int(input())
 mat = list(list(map(int, input().split())) for i in range(n))'''
 
-def fun(n):
-    keys = {'a': 0,
-            'b': 1,
-            'c': 2,
-            'd': 3,
-            'e': 4,
-            'f': 5,
-            'g': 6,
-            'h': 7}
-    mat = [['.'] * 8 for i in range(9)]
-    y = int(n[1])
-    x = int(keys.get(n[0]))
-    mat[y][x] = 'N'
-    '''
-    y from 1, j
-    x from 0 i
-    '''
-    for i in range(8):
-        for j in range(1, 9):
-            if j == y + 2 and i == x - 1 or j == y + 2 and i == x + 1 or j == y + 1 and i == x + 2 or j == y - 1 and i == x + 2 or j == y - 2 and i == x + 1 or j == y - 2 and i == x - 1 or j == y - 1 and i == x - 2 or j == y + 1 and i == x - 2:
-                mat[j][i] = '*'
+def fun(n, mat):
+    flag = 0
+    for i in range(1, n**2 + 1):
+        for j in mat:
+            if i in j:
+                flag = 1
+        if not flag: 
+            print('NO')
+            return
+        flag = 0
 
-    for i in range(-1, -len(mat), -1):
-        print(*mat[i])
+    summ = sum(mat[0])
+
+    for i in mat:
+        if summ != sum(i):
+            print('NO')
+            return
+
+    for i in range(n):
+        subsumm = 0
+        for j in range(n):
+            subsumm += mat[j][i]
+        if subsumm != summ:
+            print('NO')
+            return
+
+    summd = 0
+    for i in range(n):
+        summd += mat[i][i]
+    if summd != summ:
+        print('NO')
+        return
     
-fun('b6')
+    summd = 0
+    for i in range(n):
+        summd += mat[i-n][i]
+    if summd != summ:
+        print('NO')
+        return
+    print('YES')
+    
+fun(3, [[8, 2, 6,],
+        [3, 5, 7,],
+        [4, 9, 1]])
 print()
-fun('f3')
+fun(3, [[0, 1, 6,],
+        [3, 5, 7,],
+        [4, 9, 2]])
