9e267de8ff0ec7d9e616a301245a6ea488868d2c
108Kiselev <kiselev290@gmail.com>
2021-08-18 14:24:40 +0300

    4.6.7 заполнение

--- task.py
+++ task.py
@@ -1,17 +1,19 @@
 '''n = int(input())'''
 
 
-def fun(n):
-    mat = [[1] * n for i in range(n)]
-    
-    for i in range(n):
-        for j in range(n):
-            if i > j and i < n - 1 - j or i < j and i > n - 1 - j:
-                mat[i][j] = 0
+def fun(n, m):
+    subm = []
+    k = 1
+    for i in range(m):
+        subm.append(k)
+        k += 1
+    print(*subm)
 
-    for i in mat:
-        print(*i)
+    for i in range(n-1):
+        subm.insert(m, subm.pop(0))
+        print(*subm)
+
     
-fun(4)
+fun(5, 5)
 print()
-fun(5)
+fun(3, 7)
