0063e97ad92c5c508ebd49a204aa4311e122b2ea
108Kiselev <kiselev290@gmail.com>
2021-08-19 16:20:55 +0300

    4.7.1 сумма двух матриц

--- task.py
+++ task.py
@@ -1,66 +1,23 @@
-'''
-inp = input().split()
+'''inp = input().split()
 n = int(inp[0])
 m = int(inp[1])
-'''
 
+mat1 = [list(map(int, input().split())) for i in range(n)]
+emp = input()
+mat1 = [list(map(int, input().split())) for i in range(n)]'''
 
-def fun(n, m):
-    mat = [['.'] * m for i in range(n)]
-    coe = 0
-    k = 1
-    fl = 0
-    
-    while k <= n * m:
-        for i in range(coe, 1+coe):
-            if fl: break
-            for j in range(coe, m - coe):
-                mat[i][j] = k
-                if k == n * m: 
-                    fl = 1
-                    break
-                k += 1
+def fun(n, m, mat1, mat2):
+    matr = [['.'] * m for i in range(n)]
 
-        for i in range(1):
-            if fl: break
-            for j in range(n-1-2*coe):
-                mat[-n+1+coe+j][-1-coe] = k
-                if k == n * m: 
-                    fl = 1
-                    break
-                k += 1
-        
-        for i in range(1): #bottom
-            if fl: break
-            for j in range(-2-coe, -m-1+coe, -1):
-                mat[-1-coe][j] = k
-                if k == n * m: 
-                    fl = 1
-                    break
-                k += 1
-
-        for i in range(1):
-            if fl: break
-            for j in range(-2-coe, -n+coe, -1):
-                try: mat[j][coe] = k
-                except: pass
-                if k == n * m: 
-                    fl = 1
-                    break
-                k += 1
-        coe += 1
-        if fl or coe >= m/2: break
-                
     for i in range(n):
         for j in range(m):
-            print(str(mat[i][j]).ljust(3), end = ' ')
-        print()
-
+            matr[i][j] = mat1[i][j] + mat2[i][j]
     
-fun(4, 5)
-print()
-fun(1, 6)
-print()
-fun(3, 3)
-print()
-fun(9, 8)
+    for i in matr:
+        print(*i)
+
+
+fun(2, 4, [[1, 2, 3, 4],
+            [5, 6, 7, 1]], 
+           [[3, 2, 1, 2],
+           [1, 3, 1, 3]])
