c60fe8cf22a598c931e04104e3d376f9dcdccb7b
108Kiselev <kiselev290@gmail.com>
2021-08-20 14:58:33 +0300

    5.1.4 снежинка звездочками в квадратной матрице

--- task.py
+++ task.py
@@ -1,17 +1,22 @@
 '''n = int(input())
-mat = list(list(map(int, input().split())) for i in range(n))'''
+#mat = list(list(map(int, input().split())) for i in range(n))'''
 
-def fun(n, mat):
-    #submat = [['0'] * n for i in range(n)]
+def fun(n):
+    mat = [['.'] * n for i in range(n)]
 
     for i in range(n):
-        for j in range(n):
-            print(mat[j][i], end = ' ')
-        print()
+        mat[i][i] = '*'
+        mat[i][n-i-1] = '*'
+        mat[i][n//2] = '*'
+        mat[n//2][i] = '*'
+
+    for i in mat:
+        print(*i)
 
     
 
 
-fun(3, [[1, 2, 3],
+fun(5)
+'''fun(3, [[1, 2, 3],
         [4, 5, 6],
-        [7, 8, 9]])
+        [7, 8, 9]])'''
