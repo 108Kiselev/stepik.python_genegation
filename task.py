ac04e28c4d62b06d970c83acfc1e3b62a87a6137
108Kiselev <kiselev290@gmail.com>
2021-08-20 15:53:59 +0300

    5.1.5 симметрич матр относ побоч диаг (читерски)

--- task.py
+++ task.py
@@ -1,22 +1,23 @@
 '''n = int(input())
 #mat = list(list(map(int, input().split())) for i in range(n))'''
 
-def fun(n):
-    mat = [['.'] * n for i in range(n)]
-
-    for i in range(n):
-        mat[i][i] = '*'
-        mat[i][n-i-1] = '*'
-        mat[i][n//2] = '*'
-        mat[n//2][i] = '*'
-
-    for i in mat:
-        print(*i)
-
-    
+'''n = int(input())
+mat = list(list(map(int, input().split())) for i in range(n))
+'''
+def matrix(mat, n):
+    #j = n - i - 1 побоч диаг
+    j = 1-n
+    if mat[0][0] == mat[-1][-1]:
+        for i in range(n-1):
+            if mat[i][n - i - 1-1] != mat[i+1][n - i - 1]: return 0
+    else: return 0
 
 
-fun(5)
-'''fun(3, [[1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]])'''
+    return 1
+
+print('YES' if matrix([[0, 3, 10],
+                        [4, 9, 3],
+                        [7, 4, 0]], 3) else 'NO') #mat, n
+
+print('YES' if matrix([[1, 2],
+                        [3, 4]], 2) else 'NO')
