0ae6a1532c9dfe88280eaa237b1e5537b47f6b9b
108Kiselev <kiselev290@gmail.com>
2021-08-20 14:50:14 +0300

    5.1.3 транспонирование матрицы

--- task.py
+++ task.py
@@ -1,12 +1,17 @@
-n = int(input())
+'''n = int(input())
+mat = list(list(map(int, input().split())) for i in range(n))'''
 
-mat = list(list(map(int, input().split())) for i in range(n))
-m = mat[-n][-1]
+def fun(n, mat):
+    #submat = [['0'] * n for i in range(n)]
 
-#i > n - 1 - j
-for i in range(n):
-    for j in range(n):
-        if i >= n - 1 - j:
-            if mat[i][j] > m:
-                m = mat[i][j]
-print(m)
+    for i in range(n):
+        for j in range(n):
+            print(mat[j][i], end = ' ')
+        print()
+
+    
+
+
+fun(3, [[1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]])
