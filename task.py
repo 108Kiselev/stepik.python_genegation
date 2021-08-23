961dee23d7892209dbd3509e4c5de5622cff87fb
108Kiselev <kiselev290@gmail.com>
2021-08-23 16:19:29 +0300

    8.5.1 проверка на уникальность символов в строке

--- task.py
+++ task.py
@@ -1,15 +1,6 @@
-'''n = int(input())
-#std = [input().split() for i in range(n)]
-'''
-
-def fun(n):
-    f1, f2, f3 = 1, 1, 1
-    for i in range(n):
-        print(f1, end = ' ')
-        f1, f2, f3 = f2, f3, f1 + f2 + f3
-
-fun(10)
-print()
-fun(1)
-print()
-fun(2)
+n = int(input())
+lis = []
+for i in range(n):
+    lis.append(input().lower())
+for i in lis:
+    print(len(set(i)))
