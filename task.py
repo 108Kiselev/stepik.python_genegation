c4db4147ed7a0735bc73cbd71bb64f1b0254007b
108Kiselev <kiselev290@gmail.com>
2021-09-04 19:49:11 +0300

    10.4.1 словарь программиста

--- task.py
+++ task.py
@@ -1,10 +1,12 @@
-s = input().split()
+n = int(input())
 
-counter = {}
-for i in s:
-        if i in counter:
-                print(i + '_' + str(counter[i]), end = ' ')
-                counter[i] = counter.get(i, 0) + 1
-        else:
-                print(i, end = ' ')
-                counter[i] = counter.get(i, 0) + 1
+d = {}
+
+for i in range(n):
+        s = input().split(': ')
+        d[s[0].lower()] = s[1]
+m = int(input())
+
+for i in range(m):
+        k = input().lower()
+        print(d.get(k, 'Не найдено'))
