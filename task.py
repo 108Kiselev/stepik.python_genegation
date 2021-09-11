fdf050eedecf16ac509ec19deb27fd25a8eaecb2
108Kiselev <kiselev290@gmail.com>
2021-09-11 22:58:43 +0300

    11.2.7 список покупок с количеством

--- task.py
+++ task.py
@@ -1,13 +1,14 @@
-acts = {'write': 'W',
-        'read': 'R',
-        'execute': 'X'}
-
-files = {}
-
-for n in range(int(input())):
+d = dict()
+for _ in range(int(input())):
     i = input().split()
-    files[i[0]] = i[1:]
-
-for m in range(int(input())):
-    i = input().split()
-    print('OK' if acts.get(i[0]) in files.get(i[1]) else 'Access denied')
+    if i[0] in d:
+        if i[1] in d[i[0]]:
+            d[i[0]][i[1]] += int(i[2])
+        else: d[i[0]][i[1]] = int(i[2])
+    else: d[i[0]] = {i[1]: int(i[2])}
+sub = sorted(d)
+for s in sub:
+    print(s + ':')
+    sortedGoods = sorted(d[s])
+    for i in sortedGoods:
+        print(i + ' ' + str(d[s][i]))
