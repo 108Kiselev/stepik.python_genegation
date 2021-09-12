60ab486066d32ad5ebffa15b28fe14ffdf3dd1dc
108Kiselev <kiselev290@gmail.com>
2021-09-12 20:56:30 +0300

    12.1.1 орел или решка

--- task.py
+++ task.py
@@ -1,14 +1,4 @@
-d = dict()
+from random import *
 for _ in range(int(input())):
-    i = input().split()
-    if i[0] in d:
-        if i[1] in d[i[0]]:
-            d[i[0]][i[1]] += int(i[2])
-        else: d[i[0]][i[1]] = int(i[2])
-    else: d[i[0]] = {i[1]: int(i[2])}
-sub = sorted(d)
-for s in sub:
-    print(s + ':')
-    sortedGoods = sorted(d[s])
-    for i in sortedGoods:
-        print(i + ' ' + str(d[s][i]))
+    flag = randint(0, 1)
+    print("Орел" if flag else "Решка")
