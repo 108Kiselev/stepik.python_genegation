25ef33da0e01e4d42a5b57d4b4114836dc662b72
108Kiselev <kiselev290@gmail.com>
2021-09-12 21:35:03 +0300

    12.2.2 post code generator

--- task.py
+++ task.py
@@ -1,16 +1,9 @@
-import random
+import random as r
+import string as st
 
-length = int(input())
-st = ''
-
-for i in range(65, 91):
-    st += chr(i)
-for i in range(97, 123):
-    st += chr(i)
-
-pas = ''
-for _ in range(length):
-    i = random.randint(0, len(st))
-    pas += st[i]
- 
-print(pas)
+def generate_index():
+    #st.ascii_uppercase
+    #r.randrange(100)
+    #AB23_56VG
+    pc = r.choice(st.ascii_uppercase) + r.choice(st.ascii_uppercase) + str(r.randrange(100)) + '_' + str(r.randrange(100)) + r.choice(st.ascii_uppercase) + r.choice(st.ascii_uppercase)
+    print(pc)
