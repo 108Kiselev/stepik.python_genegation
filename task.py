30bdec9288cfeed31252a1e4b684bffb09cb893d
108Kiselev <kiselev290@gmail.com>
2021-09-26 18:53:39 +0300

    16.3.10 сортировка по гем. слова (по ключам букв)

--- task.py
+++ task.py
@@ -1,23 +1,12 @@
-import random as ran
-import string as st
+from functools import reduce
 
-#n - count
-#m - length
 
-def gen(n, m):
-    baseUp = set(st.ascii_uppercase) - set('IO')
-    baseLow = set(st.ascii_lowercase) - set('lo')
-    baseDig = set(st.digits) - set('10')
-    base = baseUp | baseLow | baseDig
-    
-    for _ in range(n):
-        pas = p = ''
-        p += ran.choice(list(baseUp))
-        p += ran.choice(list(baseLow))
-        p += str(ran.choice(list(baseDig)))
-        pas += p
-        while len(pas) < m:
-            pas += ran.choice(list(base))
-        print(pas)
+words = [input() for i in range(int(input()))]
+words = sorted(words)
+gcoe = ord('A')
 
-gen(3, 6)
+def gem(st):
+    return reduce(lambda su, s: su + ord(str(s).upper())-gcoe, st, 0)
+
+
+print(*sorted(words, key=gem), sep='\n')
