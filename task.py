4ea3ed933641c8c96690c93bbd821fda92cfefc3
108Kiselev <kiselev290@gmail.com>
2021-10-23 15:44:43 +0300

    17.4.5 загадка от Жака Фреско про цвета козлов

--- task.py
+++ task.py
@@ -1,13 +1,30 @@
-def read_csv():
-    with open('data.csv', encoding='utf-8') as file:
-        keys = file.readline().strip().split(',')
-        res = []
-        for line in file:
-            values = line.strip().split(',')
-            res.append({k: v for k, v in zip(keys, values)})
-        
-
-        print(*res, sep='\n\n')
+from decimal import Decimal as decimal
 
 
-read_csv()
+with open('goats.txt', encoding='UTF-8') as goats, open('answer.txt', 'w', encoding='UTF-8') as answer:
+    colors = dict()
+    while 1:
+        line = goats.readline().strip()
+        if line == 'COLOURS': continue
+        if line == 'GOATS': break
+        colors[line] = 0
+            
+    '''for line in goats:
+        line = line.strip()
+        if line == 'COLOURS': continue
+        if line == 'GOATS': break
+        colors[line] = 0'''
+    
+    count = 0
+    for line in goats:
+        line = line.strip()
+        colors[line] += 1
+        count += 1
+    
+    res = []
+    for key, value in colors.items():
+        colors[key] = decimal(colors[key])/decimal(count)*100
+        if colors[key] > 7: res.append(key)
+    
+    for i in sorted(res):
+        print(i, file=answer)
