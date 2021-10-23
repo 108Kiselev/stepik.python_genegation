e71e76b442cab5c4c8aac73ddc02c023c67da73d
108Kiselev <kiselev290@gmail.com>
2021-10-23 15:45:10 +0300

    17.4.6 конкатенация файлов

--- task.py
+++ task.py
@@ -1,30 +1,7 @@
-from decimal import Decimal as decimal
+names = [input() for _ in range(int(input()))]
 
-
-with open('goats.txt', encoding='UTF-8') as goats, open('answer.txt', 'w', encoding='UTF-8') as answer:
-    colors = dict()
-    while 1:
-        line = goats.readline().strip()
-        if line == 'COLOURS': continue
-        if line == 'GOATS': break
-        colors[line] = 0
-            
-    '''for line in goats:
-        line = line.strip()
-        if line == 'COLOURS': continue
-        if line == 'GOATS': break
-        colors[line] = 0'''
-    
-    count = 0
-    for line in goats:
-        line = line.strip()
-        colors[line] += 1
-        count += 1
-    
-    res = []
-    for key, value in colors.items():
-        colors[key] = decimal(colors[key])/decimal(count)*100
-        if colors[key] > 7: res.append(key)
-    
-    for i in sorted(res):
-        print(i, file=answer)
+with open('output.txt', 'w', encoding='UTF-8') as out:
+    for name in names:
+        with open(name, encoding='UTF-8') as inp:
+            data = inp.read()
+            out.write(data)
